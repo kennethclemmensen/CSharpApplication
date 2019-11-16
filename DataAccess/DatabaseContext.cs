caa5b43458d65e6fc0a6e7c83fa8a6559c731f6c
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2019-11-16 08:06:37 +0100

    Copied the code from the CodeFirstNewDatabaseSample project to the DataAccess project

---
+++ DataAccess/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+
+namespace DataAccess {
+
+    /// <summary>
+    /// The DatabaseContext represents a session to the database to query and save data
+    /// </summary>
+    public class DatabaseContext : DbContext {
+
+        /// <summary>
+        /// Initialize a new instance of the DatabaseContext class
+        /// </summary>
+        public DatabaseContext() : base("DbConnection") { }
+
+        /// <summary>
+        /// Represents the Words table in the database
+        /// </summary>
+        public DbSet<Word> Words { get; set; }
+
+        /// <summary>
+        /// Override the OnModelCreating method with the modelBuilder
+        /// to make changes to the model
+        /// </summary>
+        /// <param name="modelBuilder">The model builder</param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DataAccess/Word.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess {
+
+    /// <summary>
+    /// The Word class contains properties for a word
+    /// </summary>
+    public class Word {
+
+        /// <summary>
+        /// Get or set the Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Get or set the term
+        /// </summary>
+        [Required]
+        public string Term { get; set; }
+
+        /// <summary>
+        /// Get or set the description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
